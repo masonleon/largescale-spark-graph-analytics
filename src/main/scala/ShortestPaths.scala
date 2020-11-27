2443e37eb2094a1bc7c0a90be3d4e72cd616e9e2
April Gustafson <apriljoy@Aprils-MBP.hsd1.il.comcast.net>
2020-11-27 12:00:32 -0600

    Refactor for using social network dataset

--- src/main/scala/ShortestPaths.scala
+++ src/main/scala/ShortestPaths.scala
@@ -6,71 +6,68 @@ object ShortestPaths {
   /**
     * Number of iterations.
     */
-  val k: Int = 2      // TODO is there a better way to make sure the graph converges other than running |V| iterations?
+  val k = 2 // TODO is there a better way to make sure the graph converges other than running |V| iterations?
+  /**
+    * Weight for each edge connecting vertices in the graph.
+    */
+  val edgeWeight = 1
 
   def main(args: Array[String]): Unit = {
     val logger: org.apache.log4j.Logger = LogManager.getRootLogger
     if (args.length != 3) {
-      logger.error("Usage:\nPageRank <k value> <num iterations> <output dir>")
+      logger.error("Usage:\nShortestPaths <input> <num iterations> <output dir>")
       System.exit(1)
     }
 
-    val conf = new SparkConf().setAppName("PageRank")
+    val conf = new SparkConf().setAppName("ShortestPaths")
     val sc = new SparkContext(conf)
 
-
-    // Input file: (departID, arrivalID, distance)
+    // Input file: (userID, friendID)
     // Transform to...
-    // Graph structure:  (departID, List[(arrivalID, weight)])
+    // Graph structure:  (userID, List[(friends)])
     // Graph structure is static --> persist or cache
     val graph = sc.textFile(args(0))
       .map { line =>
-        val tokens = line.split(",") //TODO confirm file format as csv?
-      val departID = tokens(0)
-        val arrivalID = tokens(1)
-        val weight = tokens(2).toInt
-        (departID, arrivalID, weight)
-      } // If there are multiple flights for same route, they *should be* same distance (although I'm not sure that they always will be in the input data)
-      .groupBy { case (dep, arr, _) => (dep, arr) } // Get all flights with same dep/arr together
-      .mapValues(flights => flights(0)) // Take one representative flight for each
-      .map { case ((dep, arr), wt: Int) =>
-      (dep, (arr, wt))
-    }
+        val tokens = line.split("\t")
+
+        (tokens(0), tokens(1))
+      }
       .groupByKey()
       .cache()
 
-    // Distances structure: (aiportID, totalDistance to get here)
+    // Distances structure: (userID, distance)
     // This data will change each iteration
-    // Initially the totalDistance will be the minimum distance from any starting airport
-    var distances = graph.values
-      .flatMap(flight => flight)
-      .groupByKey()
-      .mapValues(distances => distances.min)
+    // Set all distances for "first hop" to 1
+    var distances = graph.values // List[(friendID)]
+      .flatMap(friends => friends.map(id => (id, edgeWeight)))
+
 
     // How to calculate the shortest path?
-    // All departure airports are in the graph.  If there exists a shortest path to anywhere, it must start at a dep airport in the graph
-    // On a shortest path, for all nodes in the route, the path is also the shortest path for those nodes
-    // So we really just need to build up the routes from every possible starting location?
+    // All origin userIDs are nodes in the graph.  If there exists a shortest path to anywhere, it must start at an ID in the graph
+    // On a shortest path, for all nodes in the path, the path is also the shortest path for those nodes
+    // So we really just need to build up the paths from every possible starting ID?
     // And then after convergence go through and output the final results?  How?
     // Will every node get covered?
-    // How could we keep track of the route for each path?
 
     for (iteration <- 0 to k) {
-      val temp = graph.join(distances)    // (airportID, ajList, totalDistance)
-        .flatMap{ case (id, (adjFlights, totalDistance)) => updateDistances(id, adjFlights, totalDistance)}
-        .reduceByKey((x, y) => Math.min(x, y))    // Only keep min distance for any airport
+      val temp = graph.rightOuterJoin(distances) // (userID, (Option[adjList], distance))
+        .flatMap {
+        case (id, (Some(adjList), distance)) => updateDistances(id, adjList, distance)
+        case (id, (None, distance)) => List((id, distance))
+      }
+        .reduceByKey((x, y) => Math.min(x, y)) // Only keep min distance for any user
     }
   }
 
   /**
-    * For all flights departing from origin airport to arrival airport, return arrival ID along with cumulative distance from origin airport.
-    * Also return origin ID with its original distance to maintain this data in the distances dataset.
-    * @param originId ID of origin airport
-    * @param adjFlights all (arrival ID, weight) for flights departing from origin airport
-    * @param distance cumulative distance to origin airport
-    * @return iterable of (airportID, cumulativeDistance) including origin and all its adjacent airports
+    * Pass the distance from given user to all of its adjacent nodes.
+    *
+    * @param userID   ID for this user
+    * @param adjList  Iterable containing IDs for this user's adjacent nodes
+    * @param distance total distance to this user
+    * @return Iterable of (userID, distance) for this user and all of its adjacent nodes.
     */
-  def updateDistances(originId: String, adjFlights: Iterable[(String, Int)], distance: Int): Iterable[(String, Int)] = {
-    adjFlights.map { case (id, wt) => (id, wt + distance)} ++ List((originId, distance))
+  def updateDistances(userID: String, adjList: Iterable[String], distance: Int): Iterable[(String, Int)] = {
+    adjList.map(id => (id, edgeWeight + distance)) ++ List((userID, distance))
   }
 }
