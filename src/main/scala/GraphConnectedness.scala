9413188905b4ece91af2634cb1b804edb33ede9d
matt <sobkowskim@gmail.com>
2020-11-27 14:13:16 -0500

    data testing

--- src/main/scala/GraphConnectedness.scala
+++ src/main/scala/GraphConnectedness.scala
@@ -5,16 +5,25 @@ object GraphConnectedness {
   def main(args: Array[String]): Unit = {
 
     val logger: org.apache.log4j.Logger = LogManager.getRootLogger
-    if (args.length != 2) {
-      logger.error("Usage:\nGraphConnectedness <input> <output>")
+    if (args.length != 1) {
+      logger.error("Usage:\nGraphConnectedness <input>")
       System.exit(1)
     }
 
-    val conf = new SparkConf().setAppName("GraphConnectedness")
+    val conf = new SparkConf().setAppName("GraphConnectedness").setMaster("local[*]")
     val sc = new SparkContext(conf)
+    val data = sc.textFile(args(0))
+
+    val res = data.map(line => line.split("\t"))
+      .map(d => (d(0), d(1)))
+      .groupByKey()
+      .map(x => (x._1, x._2.toList))
+      .sortByKey()
+
+    res.saveAsTextFile("output")
 
     // implement DFS
-    
+
 
   }
 }
