2f2fba50eb777e6d9bcbc035848e93b49b3df547
matt <sobkowskim@gmail.com>
2020-12-10 09:10:04 -0500

    filter work

--- src/main/scala/GraphStats/Cycles.scala
+++ src/main/scala/GraphStats/Cycles.scala
@@ -29,43 +29,54 @@ object Cycles {
         val nodes = line.split(" ")
         (nodes(0), nodes(1))        // (fromId, toId)
       }
-      .persist()
 
-    // "Paths" are the ways to get to another node from some node, keeping track of nodes passed on the path
-    var paths = hops.map { case (fromId, toId) => (toId, (fromId, List[String]())) }  // List will be intermediate nodes
-    var pathSize = 1
-    var maxCycleSize = 0L
-
-    while (!paths.isEmpty()) {
-      pathSize += 1
-
-      // get all paths of size "pathSize" starting at "from" ending at "to"
-      paths =
-        paths.join(hops)
-        .filter { case (middleId, ((_, intermediates), _)) => !intermediates.contains(middleId) } // Don't join on a key where you've already been!
-        .map { case (middleId, ((fromId, intermediates), toId)) =>
-          if(!intermediates.contains(middleId)){
-            (toId, (fromId, intermediates ++ List(middleId)))   // Add join key to list of intermediates
-          }else{
-            (toId, (fromId, intermediates))
-          }
-        }
-        .distinct()
-
-      // Count rows that are cycles
-      val cycles = paths.filter { case (toId, (fromId, _)) => fromId.equals(toId) }
-        .count()
-
-      if (cycles > 0) {
-        maxCycleSize = pathSize
+    var rev = input
+      .map { line =>
+        val nodes = line.split(" ")
+        (nodes(1), nodes(0))        // (toId, fromId)
       }
 
-      // Only keep rows that are NOT cycles (to avoid endlessly going in circles)
-      paths = paths.filter { case (toId, (fromId, _)) => !fromId.equals(toId) }
-    }
+    var complete = hops.leftOuterJoin(rev).filter(x => x._2._2.nonEmpty)
+      .map(node => (node._2._2.get, node._1))
 
+    logger.info(println(hops.count()))
+    logger.info(println(complete.count()))
 
-    logger.info("\n\n\n!*!*!*!*!*!*!*!*!*!**!MaxCycleSize: " + maxCycleSize.toString + "\n\n\n")
+//    // "Paths" are the ways to get to another node from some node, keeping track of nodes passed on the path
+//    var paths = hops.map { case (fromId, toId) => (toId, (fromId, List[String]())) }  // List will be intermediate nodes
+//    var pathSize = 1
+//    var maxCycleSize = 0L
+//
+//    while (!paths.isEmpty()) {
+//      pathSize += 1
+//
+//      // get all paths of size "pathSize" starting at "from" ending at "to"
+//      paths =
+//        paths.join(hops)
+//        .filter { case (middleId, ((_, intermediates), _)) => !intermediates.contains(middleId) } // Don't join on a key where you've already been!
+//        .map { case (middleId, ((fromId, intermediates), toId)) =>
+//          if(!intermediates.contains(middleId)){
+//            (toId, (fromId, intermediates ++ List(middleId)))   // Add join key to list of intermediates
+//          }else{
+//            (toId, (fromId, intermediates))
+//          }
+//        }
+//        .distinct()
+//
+//      // Count rows that are cycles
+//      val cycles = paths.filter { case (toId, (fromId, _)) => fromId.equals(toId) }
+//        .count()
+//
+//      if (cycles > 0) {
+//        maxCycleSize = pathSize
+//      }
+//
+//      // Only keep rows that are NOT cycles (to avoid endlessly going in circles)
+//      paths = paths.filter { case (toId, (fromId, _)) => !fromId.equals(toId) }
+//    }
+//
+//
+//    logger.info("\n\n\n!*!*!*!*!*!*!*!*!*!**!MaxCycleSize: " + maxCycleSize.toString + "\n\n\n")
 //    sc.parallelize(Seq(maxCycleSize))
 //      .coalesce(1)
 //      .saveAsTextFile(args(1))
