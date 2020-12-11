42efe7c49790a3f42e0f31c275985ee3cf1ccae8
April Gustafson <apriljoy@Aprils-MacBook-Pro.local>
2020-12-11 09:23:34 -0600

    Cleanup all APSP versions

--- src/main/scala/ShortestPath/Version2_3.scala
+++ src/main/scala/ShortestPath/Version2_3.scala
@@ -1,10 +1,10 @@
 package ShortestPath
 
-import ShortestPath.ShortestPaths.{generateGraphRDD, initializeDistances, saveSingleOutput, updateDistances}
+import PathUtils.{generateGraphRDD, initializeDistances, updateDistances}
 import org.apache.log4j.LogManager
 import org.apache.spark.{SparkConf, SparkContext}
 
-object ShortestPathsConvergence {
+object Version2_3 {
 
   /**
    * Weight for each edge connecting vertices in the graph.
@@ -27,16 +27,17 @@ object ShortestPathsConvergence {
 
     // Distances structure: ((toId, fromId), distance)
     // This data will change each iteration
-    var distances = initializeDistances(graph, optimizeJoin)
+    var distances = initializeDistances(graph, edgeWeight, optimizeJoin)
 
-    var iterCount = 0
+    val iterCount = sc.accumulator(0)
     var numUpdated = 1L
     while (numUpdated > 0) {
       val temp = distances.map { case ((toId, fromId), distance) => (toId, (fromId, distance)) }
         .leftOuterJoin(graph) // (toId, ((fromId, distance), Option[adjList]))
-        .flatMap(x => updateDistances(x))
+        .flatMap(x => updateDistances(x, edgeWeight))
         .reduceByKey((x, y) => Math.min(x, y)) // Only keep min distance for any (to, from) pair
 
+      // Count the number of distances in any path that have been updated and new paths added
       numUpdated = temp.leftOuterJoin(distances) // ((to, from), newDist, Option[oldDist])
         .filter {
           case (_, (newDist, Some(oldDist))) => newDist != oldDist
@@ -48,6 +49,8 @@ object ShortestPathsConvergence {
     }
 
     logger.info("!*!*!*Number of iterations: " + iterCount.toString)
-    saveSingleOutput(distances, args(1) + "/convergedDistances")
+    distances.saveAsTextFile(args(1) + "/convergedDistances")
+
+    sc.parallelize(Seq(iterCount.value)).coalesce(1).saveAsTextFile(args(1) + "/diameter")
   }
 }
